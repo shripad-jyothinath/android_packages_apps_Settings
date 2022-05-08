613d34e4dd5085a80abf68caa0ed2aeddd0cf987
StarkDroid <gamerprince.exp@gmail.com>
2022-05-08 00:24:07 +0000

    QSTransparency: Fire SystemUI restart
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>

--- src/com/android/settings/corvus/fragments/QuickSettings.java
+++ src/com/android/settings/corvus/fragments/QuickSettings.java
@@ -31,9 +31,17 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settingslib.search.SearchIndexable;
 
+import com.android.internal.util.corvus.CorvusUtils;
+
+import com.corvus.support.preferences.SystemSettingSeekBarPreference;
+
 @SearchIndexable
 public class QuickSettings extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
+
+    private static final String QS_TRANSPARENCY = "qs_transparency";
+
+    private SystemSettingSeekBarPreference mQsTransparency;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,10 +51,18 @@ public class QuickSettings extends SettingsPreferenceFragment
         addPreferencesFromResource(R.xml.quicksettings);
 
         final PreferenceScreen screen = getPreferenceScreen();
+
+        mQsTransparency = (SystemSettingSeekBarPreference) findPreference(QS_TRANSPARENCY);
+        mQsTransparency.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mQsTransparency) {
+            CorvusUtils.showSystemUiRestartDialog(getActivity());
+            return true;
+        }
         return false;
     }
 
